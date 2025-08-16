0dd5efac18056c02671e9e49845510c82b283de5
kumar6646 <sai.penusoft@gmail.com>
2025-08-16 10:26:19 +0530

    Add project files.

---
+++ FirstConsoleApplicatio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApplicatio
+{
+    internal class Program
+    {
+        /*
+         * Comments are of two types:
+         * Single Line Comments where we can comment the code in a single line.
+         * and code is written in a single line with (//) this is 
+         * single line comments only one line we can comment it.
+         * 
+         * Multi line comments where we can comment the code more than one 
+         * line at a time by unsing(/*and wrting the commentented code and ending 
+         * with ) so it is considered as multiline
+         * 
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Write("Welcome");
+            Console.WriteLine("to c#");
+
+        }
+    }
+}
